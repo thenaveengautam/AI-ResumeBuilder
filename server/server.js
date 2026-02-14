8da178c6b7b3b6143e8a8eb6cf427af681dca8e4
thenaveengautam <dmnaveengautam@gmail.com>
2026-02-14 11:09:03 +0530

    feat: add production url in server.js

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ app.use(cors({
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:3000",
+        "http://cvforge.strangled.net",
     ],
     credentials: true,
 }))
