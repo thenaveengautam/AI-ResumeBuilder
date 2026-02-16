12427eb5e7a115718ba7f961dd12c4ceff0518ce
thenaveengautam <dmnaveengautam@gmail.com>
2026-02-16 10:48:50 +0530

    fixed cors error

--- server/server.js
+++ server/server.js
@@ -18,6 +18,7 @@ app.use(cors({
         "http://localhost:5173",
         "http://localhost:5174",
         "http://localhost:3000",
+        "https://ai-cvforge.onrender.com"
     ],
     credentials: true,
 }))
